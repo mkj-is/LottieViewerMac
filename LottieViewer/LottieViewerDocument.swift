cb05b4ef6443982eb17df6becf0ffa416a5682aa
Matěj Kašpar Jirásek <matej.jirasek@me.com>
2025-01-04 15:32:41 +0100

    Support loading .lottie files

--- LottieViewer/LottieViewerDocument.swift
+++ LottieViewer/LottieViewerDocument.swift
@@ -23,6 +23,9 @@ struct LottieViewerDocument: FileDocument {
 
     enum FileWrapperError: Error {
         case writeNotSupported
+        case noFilename
+        case noDotLottieAnimation
+        case unknownContentType
     }
 
     let animation: LottieAnimation
@@ -34,7 +37,24 @@ struct LottieViewerDocument: FileDocument {
         guard let data = configuration.file.regularFileContents else {
             throw CocoaError(.fileReadCorruptFile)
         }
-        animation = try LottieAnimation.from(data: data)
+        if configuration.contentType == .lottie {
+            animation = try LottieAnimation.from(data: data)
+        } else if configuration.contentType == .dotLottie {
+            animation = try LottieViewerDocument.loadDotLottie(configuration: configuration, data: data)
+        } else {
+            throw FileWrapperError.unknownContentType
+        }
+    }
+
+    private static func loadDotLottie(configuration: ReadConfiguration, data: Data) throws -> LottieAnimation {
+        guard let filename = configuration.file.filename else {
+            throw FileWrapperError.noFilename
+        }
+        let dotLottieFile = try DotLottieFile.SynchronouslyBlockingCurrentThread.loadedFrom(data: data, filename: filename).get()
+        guard let firstAnimation = dotLottieFile.animations.first else {
+            throw FileWrapperError.noDotLottieAnimation
+        }
+        return firstAnimation.animation
     }
 
     func fileWrapper(configuration: WriteConfiguration) throws -> FileWrapper {
