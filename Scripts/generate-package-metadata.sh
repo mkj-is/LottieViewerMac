179de1361dc5ff56b93031f9297f995a9c27b011
Matěj Kašpar Jirásek <matej.jirasek@me.com>
2025-02-16 22:58:53 +0100

    Fix generating package data during archival

--- Scripts/generate-package-metadata.sh
+++ Scripts/generate-package-metadata.sh
@@ -1,22 +1,28 @@
 #!/bin/bash
 
-# Check if output directory is provided
-if [ $# -eq 0 ]; then
-    echo "Error: BUILD_DIR data directory not specified"
-    echo "Usage: $0 <output_directory>"
+if [ $# -lt 2 ]; then
+    echo "Error: BUILD_DIR or BUILT_PRODUCTS_DIR directory not specified"
+    echo "Usage: $0 <BUILD_DIR> <BUILT_PRODUCTS_DIR>"
     exit 1
 fi
 
 BUILD_DIR="$1"
+BUILT_PRODUCTS_DIR="$2"
+
+# Path during archive
+ARCHIVE_SPM_DIR="${BUILT_PRODUCTS_DIR}/SwiftPackageProducts"
+
+# Path during build
+BUILD_SPM_DIR="${BUILD_DIR%/Build/*}/SourcePackages/checkouts"
+
+
+if [ -d "$ARCHIVE_SPM_DIR" ]; then
+    TARGET_SPM_DIR="$ARCHIVE_SPM_DIR"
+else
+    TARGET_SPM_DIR="$BUILD_SPM_DIR"
+fi
 
 JSON_FILE="LottieViewer.xcodeproj/project.xcworkspace/xcshareddata/swiftpm/Package.resolved"
-
-# Find the LICENSE file in SPM cache
-LOTTIE_LICENSE_PATH="${BUILD_DIR}/../../SourcePackages/checkouts/lottie-ios/LICENSE"
-DOTLOTTIE_LICENSE_PATH="${BUILD_DIR}/../../SourcePackages/checkouts/dotlottie-ios/LICENSE"
-
-LOTTIE_LICENSE=$(cat "$LOTTIE_LICENSE_PATH" | sed ':a;N;$!ba;s/\n/\\n/g')
-DOTLOTTIE_LICENSE=$(cat "$LOTTIE_LICENSE_PATH" | sed ':a;N;$!ba;s/\n/\\n/g')
 
 # Output Swift file
 SWIFT_FILE="LottieViewer/Generated/ResolvedPackages.swift"
@@ -43,7 +49,7 @@ jq -c '.pins[]' "$JSON_FILE" | while read -r pin; do
     VERSION=$(echo "$pin" | jq -r '.state.version')
 
     # Construct the license path based on the identity
-    LICENSE_PATH="${BUILD_DIR}/../../SourcePackages/checkouts/${IDENTITY}/LICENSE"
+    LICENSE_PATH="${TARGET_SPM_DIR}/${IDENTITY}/LICENSE"
     LICENSE=$(sed ':a;N;$!ba;s/\n/\\n/g' "$LICENSE_PATH")
 
     cat <<EOF >> "$SWIFT_FILE"
