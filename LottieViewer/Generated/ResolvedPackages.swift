cdf34eca0a3f107307b386963240ec1921765326
Matěj Kašpar Jirásek <matej.jirasek@me.com>
2025-05-26 10:13:30 +0200

    Update packages

--- LottieViewer/Generated/ResolvedPackages.swift
+++ LottieViewer/Generated/ResolvedPackages.swift
@@ -11,7 +11,7 @@ enum ResolvedPackages {
     static let dictionary: [String: Package] = [
         "dotlottie-ios": Package(
             location: URL(string: "https://github.com/LottieFiles/dotlottie-ios")!,
-            version: "0.8.0",
+            version: "0.8.1",
             license: """
 MIT License
 
@@ -245,7 +245,7 @@ SOFTWARE.
         ),
         "rive-ios": Package(
             location: URL(string: "https://github.com/rive-app/rive-ios")!,
-            version: "6.8.0",
+            version: "6.8.2",
             license: """
 MIT License
 
