42ed145adf6dfee12f73061cfe8e5f8faa9f2a3d
Matěj Kašpar Jirásek <matej.jirasek@me.com>
2026-01-18 14:41:45 +0100

    Update dotlottie-ios and rive-ios package versions
    Bumped dotlottie-ios to v0.12.1 and rive-ios to v6.13.0 in Package.resolved and ResolvedPackages.swift to use the latest releases.

--- LottieViewer/Generated/ResolvedPackages.swift
+++ LottieViewer/Generated/ResolvedPackages.swift
@@ -11,7 +11,7 @@ enum ResolvedPackages {
     static let dictionary: [String: Package] = [
         "dotlottie-ios": Package(
             location: URL(string: "https://github.com/LottieFiles/dotlottie-ios")!,
-            version: "0.12.0",
+            version: "0.12.1",
             license: """
 MIT License
 
@@ -245,7 +245,7 @@ SOFTWARE.
         ),
         "rive-ios": Package(
             location: URL(string: "https://github.com/rive-app/rive-ios")!,
-            version: "6.12.3",
+            version: "6.13.0",
             license: """
 MIT License
 
